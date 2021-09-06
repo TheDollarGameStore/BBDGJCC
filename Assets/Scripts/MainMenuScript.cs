f3f00daa5ba61107aecf85c1694aa2625e19bde5
Louis Strauss <ljstrss@gmail.com>
2021-09-06 17:20:15 +0200

    Spruced up Characters Menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,12 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Image selectedCharacterImage;
+    public Button character1Button;
+    public Button character2Button;
+    public Button character3Button;
+    public Button character4Button;
+    public Button character5Button;
+    public Button character6Button;
+    public Button character7Button;
+    public Button character8Button;
     public Slider loadingSlider;
+    private string lockedText = "LOCKED";
+    private string[] characterTextList = new string[8];
+    private Button[] characterButtonList = new Button[8];
+    private ColorBlock enabledColourBlock = new ColorBlock()
+    {
+        normalColor = new Color(1, 1, 1, 0.75f),
+        fadeDuration = 0.1f,
+        pressedColor = new Color(1, 1, 1, 1),
+        selectedColor = new Color(1, 1, 1, 1),
+        colorMultiplier = 1,
+        highlightedColor = new Color(1, 1, 1, 1)
+    };
+    private ColorBlock disabledColourBlock = new ColorBlock()
+    {
+        normalColor = new Color(0, 0, 0, 0.25f),
+        fadeDuration = 0.1f,
+        pressedColor = new Color(0, 0, 0, 0.5f),
+        selectedColor = new Color(0, 0, 0, 0.5f),
+        colorMultiplier = 1,
+        highlightedColor = new Color(0, 0, 0, 0.5f)
+    };
+    public TextMeshProUGUI selectedCharacterText;
+
+    public void Start()
+    {
+        InitializeCharacter(0, character1Button, PlayerPrefs.HasKey("unlockedKabbage"), "Cabbitsu, cabbitsu, cab-bit-su. LETTUSU, LETTUSU, LET-TU-SUHHH! Kabbage is an adorable little sprite that didn't quite make our lineup, but they were simply too cute not to share. Hope they make you smile.");
+        InitializeCharacter(1, character2Button, PlayerPrefs.HasKey("unlockedTina"), "Tina turnip turns UP. This proactive, purple pisces is the heart of the party. Always willing to lend a hand or an ear, you'll find it's her words of encouragement that push you to make the right decision every time. She's simply the best!");
+        InitializeCharacter(2, character3Button, PlayerPrefs.HasKey("unlockedCollie"), "Broski, Brogle, Bro Collie goes by many names, but doesn't matter which you pick, he'll come to your beck and call. Big hair, big vibes and biiig flatulence are just some of the words used to describe your funky, floral, fibrous friend.");
+        InitializeCharacter(3, character4Button, PlayerPrefs.HasKey("unlockedGary"), "Pee-ew, who invited Gary Garlic? Why, you did of course, because of his amazing health benefits! Just a shame about the smell... Best to avoid any face to face conversations for a while. And to think, all Gary really wanted was a hug.");
+        InitializeCharacter(4, character5Button, PlayerPrefs.HasKey("unlockedTommy"), "Tommy's therapist keeps telling him, if you keep everything bottled up inside, one day you'll explode! And judging by how quiet he's been lately, and how red he's getting in the face... You know what? I'm just going to take a step back...");
+        InitializeCharacter(5, character6Button, PlayerPrefs.HasKey("unlockedPatrick"), "Tantalise your tastebuds with the mouthwatering perfection that is Patrick. He's thicc, he's juicy, he's large and in charge. Serving you body-ody-ody with a side of fries. He'll probably leave you wanting less, but it's too late now. Oh lard he comin!");
+        InitializeCharacter(6, character7Button, PlayerPrefs.HasKey("unlockedSteve"), "STEVE!");
+        InitializeCharacter(7, character8Button, PlayerPrefs.HasKey("unlockedIda"), "Ice cold Ida they call her, and with good reason... To the world it would seem like her heart is frozen. But when it's just her on the couch in front of a soppy romcom, she eats her feelings and thinks about the cone that got away.");
+    }
 
     public void PlayGame()
     {
@@ -16,6 +60,22 @@ public class MainMenuScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SelectCharacter(int index)
+    {
+        Button button = characterButtonList[index];
+        button.Select();
+        selectedCharacterText.text = characterTextList[index];
+        selectedCharacterImage.color = button.colors.highlightedColor;
+        selectedCharacterImage.sprite = button.GetComponentsInChildren<Image>()[1].sprite;
+    }
+
+    private void InitializeCharacter(int index, Button button, bool unlocked, string text)
+    {
+        button.colors = unlocked ? enabledColourBlock : disabledColourBlock;
+        characterTextList[index] = unlocked ? text : lockedText;
+        characterButtonList[index] = button;
     }
 
     private IEnumerator LoadScene(int sceneBuildIndex)

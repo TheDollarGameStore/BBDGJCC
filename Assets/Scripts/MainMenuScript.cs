22cd2c60614a17165d06f1e222cad302f7489c98
Louis Strauss <ljstrss@gmail.com>
2021-09-08 21:23:00 +0200

    Reworked Level Select

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -7,9 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private int currentLevel;
+    private int unlockedLevel = 1;
     public Image selectedCharacterImage;
     public Image selectedAchievementImage;
     public Button endlessButton;
+    public Button nextLevelButton;
     public Button character1Button;
     public Button character2Button;
     public Button character3Button;
@@ -18,11 +21,6 @@ public class MainMenuScript : MonoBehaviour
     public Button character6Button;
     public Button character7Button;
     public Button character8Button;
-    public Button level1NextButton;
-    public Button level2NextButton;
-    public Button level3NextButton;
-    public Button level4NextButton;
-    public Button level5NextButton;
     public Button achievement1Button;
     public Button achievement2Button;
     public Button achievement3Button;
@@ -31,6 +29,7 @@ public class MainMenuScript : MonoBehaviour
     public Button achievement6Button;
     public Button achievement7Button;
     public Button achievement8Button;
+    public Button previousLevelButton;
     public Slider loadingSlider;
     private string lockedText = "LOCKED";
     private string[] characterTextList = new string[8];
@@ -55,6 +54,7 @@ public class MainMenuScript : MonoBehaviour
         colorMultiplier = 1,
         highlightedColor = new Color(0, 0, 0, 0.5f)
     };
+    public TextMeshProUGUI currentLevelText;
     public TextMeshProUGUI selectedCharacterText;
     public TextMeshProUGUI selectedAchievementText;
 
@@ -64,24 +64,26 @@ public class MainMenuScript : MonoBehaviour
         PlayerPrefs.SetInt("unlockedPatrick", 1);
         if (PlayerPrefs.HasKey("unlockedTina"))
         {
-            level1NextButton.interactable = true;
+            unlockedLevel++;
         }
         if (PlayerPrefs.HasKey("unlockedGary"))
         {
-            level2NextButton.interactable = true;
+            unlockedLevel++;
         }
         if (PlayerPrefs.HasKey("unlockedSteve"))
         {
-            level3NextButton.interactable = true;
+            unlockedLevel++;
         }
         if (PlayerPrefs.HasKey("unlockedTommy"))
         {
-            level4NextButton.interactable = true;
+            unlockedLevel++;
         }
         if (PlayerPrefs.HasKey("unlockedIda"))
         {
-            level5NextButton.interactable = true;
+            unlockedLevel++;
         }
+        currentLevel = unlockedLevel;
+        UpdateLevelGameObjects();
         if (PlayerPrefs.HasKey("unlockedCabbitsu"))
         {
             endlessButton.interactable = true;
@@ -90,7 +92,7 @@ public class MainMenuScript : MonoBehaviour
         InitializeCharacter(1, character2Button, PlayerPrefs.HasKey("unlockedTina"), "Tina turnip turns UP. This proactive, purple pisces is the heart of the party. Always willing to lend a hand or an ear, you'll find it's her words of encouragement that push you to make the right decision every time. She's simply the best!");
         InitializeCharacter(2, character3Button, PlayerPrefs.HasKey("unlockedGary"), "Pee-ew, who invited Gary Garlic? Why, you did of course, because of his amazing health benefits! Just a shame about the smell... Best to avoid any face to face conversations for a while. And to think, all Gary really wanted was a hug.");
         InitializeCharacter(3, character4Button, PlayerPrefs.HasKey("unlockedTommy"), "Tommy's therapist keeps telling him, if you keep everything bottled up inside, one day you'll explode! And judging by how quiet he's been lately, and how red he's getting in the face... You know what? I'm just going to take a step back...");
-        InitializeCharacter(4, character5Button, PlayerPrefs.HasKey("unlockedCabbitsu"), "Cabbitsu, cabbitsu, cab-bit-su. LETTUSU, LETTUSU, LET-TU-SUHHH! Kabbage is an adorable little sprite that didn't quite make our lineup, but they were simply too cute not to share. Hope they make you smile.");
+        InitializeCharacter(4, character5Button, PlayerPrefs.HasKey("unlockedCabbitsu"), "Cabbitsu, cabbitsu, cab-bit-su. LETTUSU, LETTUSU, LET-TU-SUHHH! Cabbitsu is the official Broccoli vs Burgers mascot. They will guide you through the game, celebrate your successes and acknowledge your failures. Make them proud!");
         InitializeCharacter(5, character6Button, PlayerPrefs.HasKey("unlockedPatrick"), "Tantalise your tastebuds with the mouthwatering perfection that is Patrick. He's thicc, he's juicy, he's large and in charge. Serving you body-ody-ody with a side of fries. He'll probably leave you wanting less, but it's too late now. Oh lard he comin!");
         InitializeCharacter(6, character7Button, PlayerPrefs.HasKey("unlockedSteve"), "STEVE!");
         InitializeCharacter(7, character8Button, PlayerPrefs.HasKey("unlockedIda"), "Ice cold Ida they call her, and with good reason... To the world it would seem like her heart is frozen. But when it's just her on the couch in front of a soppy romcom, she eats her feelings and thinks about the cone that got away.");
@@ -104,15 +106,33 @@ public class MainMenuScript : MonoBehaviour
         InitializeAchievement(7, achievement8Button, PlayerPrefs.HasKey("earnedLaTomatina"), "LA TOMATINA\nComplete a level using only turnips and tomatoes");
     }
 
-    public void PlayGame(int level)
-    {
-        PlayerPrefs.SetInt("currentLevel", level);
-        StartCoroutine(LoadScene(1));
-    }
-
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void NextLevel()
+    {
+        currentLevel++;
+        UpdateLevelGameObjects();
+    }
+
+    public void PlayLevel()
+    {
+        PlayerPrefs.SetInt("currentLevel", currentLevel);
+        StartCoroutine(LoadScene(1));
+    }
+
+    public void PlayEndless()
+    {
+        PlayerPrefs.SetInt("currentLevel", -1);
+        StartCoroutine(LoadScene(1));
+    }
+
+    public void PreviousLevel()
+    {
+        currentLevel--;
+        UpdateLevelGameObjects();
     }
 
     public void SelectCharacter(int index)
@@ -145,6 +165,13 @@ public class MainMenuScript : MonoBehaviour
         button.colors = unlocked ? enabledColourBlock : disabledColourBlock;
         achievementTextList[index] = text;
         achievementButtonList[index] = button;
+    }
+
+    public void UpdateLevelGameObjects()
+    {
+        currentLevelText.text = "LEVEL " + currentLevel;
+        nextLevelButton.interactable = currentLevel != unlockedLevel;
+        previousLevelButton.interactable = currentLevel != 1;
     }
 
     private IEnumerator LoadScene(int sceneBuildIndex)

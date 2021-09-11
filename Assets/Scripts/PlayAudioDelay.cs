0dd5ade00954d1a68c7be1b4b71c7a9c992b7911
NodziGames <nodzigames@gmail.com>
2021-09-11 14:49:55 +0200

    Made sound event public

--- Assets/Scripts/PlayAudioDelay.cs
+++ Assets/Scripts/PlayAudioDelay.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PlayAudioDelay : MonoBehaviour
 {
-    public float delay;
-
     private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        Invoke("PlaySound", delay);
     }
 
     // Update is called once per frame
-    void PlaySound()
+    public void PlaySound()
     {
         audioSource.Play();
     }
